03ddf8d19eb8b659f69753c71b5259e8883a38ec
Nihel51 <Nihel.Hassad@Esprit.tn>
2026-02-14 19:55:09 +0100

    Ajout partie backend DetectionMaladie

---
+++ backend/Detection_Maladie-Service/src/main/java/Fakerni/Detection_Maladie_Service/DetectionMaladieServiceApplication.java
@@ -0,0 +1,13 @@
+package Fakerni.Detection_Maladie_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectionMaladieServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectionMaladieServiceApplication.class, args);
+	}
+
+}
